5fe90d31f3cce7662f9786fd703ec3bf88faae40
Aidana Akpaeva <akpaeva02@gmail.com>
2022-10-10 13:47:28 +0500

    change main.js

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ getFile('data.json');//путь к файлу
 // creating variables
 let main__container = document.getElementById('main__container');
 let categories = document.getElementById('categories');
+
+categories.classList.add('tab');
 let movies = document.getElementById('movies');
 
 function parse(obj) {
@@ -21,33 +23,13 @@ function parse(obj) {
   let id_movies;
 
   DATA = JSON.parse(obj);
-  for (let i in DATA.categories) {
-    for (let name_categories in DATA.categories[i]) {
-      let btn = document.createElement('button')
-      btn.innerHTML = DATA.categories[i][name_categories];
-      btn.id = `btn_${name_categories}`;
-
-      btn.onclick = function () {  // замена фильмов на выбранный жанр
-        let id = btn.id;
-        id.replace('btn_', '');// удалить первые черыре символа и остальное будет жанром
-        main__container.innerHTML = '';
-        main__container.appendChild(document.getElementById(id));
-      }
-      
-      if (DATA.categories[i][name_categories] == DATA.categories[i].detective) {
-        categories.appendChild(btn).classList.add('wrapper__pagination-active');
-      }
-      categories.appendChild(btn).classList.add('wrapper__pagination');
-      let id_categories_transition = document.getElementById(`btn_${name_categories}`);
-      id_categories = id_categories_transition;
-    }
-  }
   
   for (let film in DATA.movies) {
     // в цикле ниже добавляем все элементы в tab, а не в main__container
     for (let genre in DATA.movies[film]) {
       for (let film_info in DATA.movies[film][genre]) {
         let tab_name_movies = document.createElement('div');
+        let tab_movies_container = document.createElement('div');
         let img_movies = document.createElement('div');
         let main_movies = document.createElement('div');
         let tab_descr_movies = document.createElement('div');
@@ -61,8 +43,11 @@ function parse(obj) {
         let li_4 = document.createElement('li');
         let li_5 = document.createElement('li');
 
-        movies.appendChild(tab_name_movies).id = `tab_${genre}`;
-        tab_name_movies.classList.add('tab_name_movies');
+        movies.appendChild(tab_movies_container).classList.add('tabcontent');
+        tab_name_movies.id = `tab_${genre}`;
+
+          tab_movies_container.appendChild(tab_name_movies);
+      
         tab_name_movies.appendChild(main_movies).id = 'main_movies';
         main_movies.appendChild(tab_descr_movies).id = 'tab_descr_movies';
 
@@ -96,14 +81,42 @@ function parse(obj) {
     }
   }
 
-  for(let elem in DATA) {
-    if (id_categories[elem].categories === id_movies[elem].movies) {
-      console.log('test');
+  for (let i in DATA.categories) {
+    for (let name_categories in DATA.categories[i]) {
+      let btn = document.createElement('button')
+      btn.innerHTML = DATA.categories[i][name_categories];
+      btn.id = `btn_${name_categories}`;
+      btn.onclick = function openCategories(evt) {
+        // Declare all variables
+        console.log('111');
+        var i, tabcontent, tablinks;
+      
+        // Get all elements with class="tabcontent" and hide them
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+          tabcontent[i].style.display = "none";
+        }
+      
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+      
+        // Show the current tab, and add an "active" class to the button that opened the tab
+        document.getElementById(`btn_${name_categories}`).style.display = "block";
+        evt.currentTarget.className += " active";
+      } 
+      
+      categories.appendChild(btn).classList.add('wrapper__pagination', 'tablinks');
+      let id_categories_transition = document.getElementById(`btn_${name_categories}`);
+      id_categories = id_categories_transition;
     }
+    
   }
 
-
 }
+
 
 
 // preloader
